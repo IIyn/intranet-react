7c190d8f75d18e9c7378fb150e591e31d490ff31
IIyn <yassinam.pro@gmail.com>
2023-01-10 17:19:17 +0100

    Fixed connection issue

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -37,44 +37,59 @@ const Login = () => {
 
   return (
     <div>
-        <div>
-            <h1 className="p-5 font-bold text-6xl">Connexion</h1>
-            <div className="divide-y-4"/>
-            <p className="p-3">Pour vous connecter à l'intranet, entrez votre identifiant et mot de passe</p>
-        </div>
+      <div>
+        <h1 className="p-5 font-bold text-6xl">Connexion</h1>
+        <div className="divide-y-4" />
+        <p className="p-3">
+          Pour vous connecter à l'intranet, entrez votre identifiant et mot de
+          passe
+        </p>
+      </div>
 
-      <form className="flex flex-col"
+      <form
+        className="flex flex-col"
         onSubmit={(e) => {
           e.preventDefault();
           handleSubmit();
         }}
       >
-          <div className="flex flex-col">
-              <div className="p-2 flex justify-end">
-                  <label className="flex items-center" htmlFor="email">Email :</label>
-                  <input className="p-2 ml-2  w-3/4"
-                      type="text"
-                      name="email"
-                      placeholder="ex: owen.lopez@example.com"
-                      id="email"
-                      onChange={(e) => {
-                          setEmail(e.target.value);
-                      }}
-                  />
-              </div>
-              <div className="p-2 flex justify-end ">
-                  <label className="flex items-center" htmlFor="password">Mot de passe :</label>
-                  <input className="p-2 ml-2 w-3/4"
-                      type="text"
-                      name="password"
-                      onChange={(e) => {
-                          setPassword(e.target.value);
-                      }}
-                  />
-              </div>
+        <div className="flex flex-col">
+          <div className="p-2 flex justify-end">
+            <label className="flex items-center" htmlFor="email">
+              Email :
+            </label>
+            <input
+              className="p-2 ml-2  w-3/4"
+              type="text"
+              name="email"
+              placeholder="ex: owen.lopez@example.com"
+              id="email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
           </div>
+          <div className="p-2 flex justify-end ">
+            <label className="flex items-center" htmlFor="password">
+              Mot de passe :
+            </label>
+            <input
+              className="p-2 ml-2 w-3/4"
+              type="text"
+              name="password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+        <button
+          className="w-1/4 p-3 mt-5 font-bold bg-gradient-to-b from-red-400 via-red-500 to-pink-500 "
+          type="submit"
+        >
+          Connexion
+        </button>
       </form>
-        <button className="w-1/4 p-3 mt-5 font-bold bg-gradient-to-b from-red-400 via-red-500 to-pink-500 " type="submit">Connexion</button>
     </div>
   );
 };
