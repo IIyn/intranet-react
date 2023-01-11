6d50e62d765794cc7bec621eefb0bb7f312271f8
Tiphaine JUQUEL <Tiphaine.juquel@gmail.com>
2023-01-11 12:00:25 +0100

    [Front] - Modify CSS for button login and add Title "Intranet"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,78 +1,83 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { selectUser } from "../slices/userSlice";
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {selectUser} from "../slices/userSlice";
 
 import ProfilePopUp from "./ProfilePopUp";
 
 const Header = () => {
-  const { user } = useSelector(selectUser);
-  const [showProfilePopUp, setShowProfilePopUp] = useState({
-    show: false,
-    type: "",
-  });
-  const navigate = useNavigate();
-  return (
-    <header className=" w-full h-12 bg-gradient-to-b from-red-400 via-red-500 to-pink-500">
-      {user ? (
-        <>
-          <button
-            onClick={() => {
-              navigate("/search");
-            }}
-          >
-            Chercher un collaborateur
-          </button>
-          {user.isAdmin && (
-            <button
-              onClick={() => {
-                setShowProfilePopUp({
-                  show: true,
-                  type: "Ajouter un profil",
-                });
-              }}
-            >
-              Ajouter un profil
-            </button>
-          )}
-          <img
-            src={user.photo}
-            alt="user-image"
-            onClick={() => {
-              setShowProfilePopUp({
-                show: true,
-                type: "Modifier mon profil",
-              });
-            }}
-          />
-          <button
-            onClick={() => {
-              localStorage.removeItem("user");
-              localStorage.removeItem("token");
-              navigate("/login");
-              window.location.reload();
-            }}
-          >
-            Disconnect
-          </button>
-          {showProfilePopUp.show && (
-            <ProfilePopUp
-              setShowProfilePopUp={setShowProfilePopUp}
-              type={showProfilePopUp.type}
-            />
-          )}
-        </>
-      ) : (
-        <button className="p-1 text-2xl"
-          onClick={() => {
-            navigate("/login");
-          }}
-        >
-          Connexion
-        </button>
-      )}
-    </header>
-  );
+    const {user} = useSelector(selectUser);
+    const [showProfilePopUp, setShowProfilePopUp] = useState({
+        show: false,
+        type: "",
+    });
+    const navigate = useNavigate();
+    return (
+        <header className="flex justify-between w-full h-12 bg-gradient-to-b from-red-400 via-red-500 to-pink-500">
+            <div className="p-1 pl-7">
+                <p className="font-bold text-3xl">Intranet</p>
+            </div>
+            {user ? (
+                <>
+                    <button
+                        onClick={() => {
+                            navigate("/search");
+                        }}
+                    >
+                        Chercher un collaborateur
+                    </button>
+                    {user.isAdmin && (
+                        <button
+                            onClick={() => {
+                                setShowProfilePopUp({
+                                    show: true,
+                                    type: "Ajouter un profil",
+                                });
+                            }}
+                        >
+                            Ajouter un profil
+                        </button>
+                    )}
+                    <img
+                        src={user.photo}
+                        alt="user-image"
+                        onClick={() => {
+                            setShowProfilePopUp({
+                                show: true,
+                                type: "Modifier mon profil",
+                            });
+                        }}
+                    />
+                    <button
+                        onClick={() => {
+                            localStorage.removeItem("user");
+                            localStorage.removeItem("token");
+                            navigate("/login");
+                            window.location.reload();
+                        }}
+                    >
+                        Disconnect
+                    </button>
+                    {showProfilePopUp.show && (
+                        <ProfilePopUp
+                            setShowProfilePopUp={setShowProfilePopUp}
+                            type={showProfilePopUp.type}
+                        />
+                    )}
+                </>
+            ) : (
+                <div className="p-1 px-7 bg-slate-200 text-red-500 hover:bg-rose-300 hover:text-white">
+                    <button className="mt-1 font-bold text-2xl"
+                            onClick={() => {
+                                navigate("/login");
+                            }}
+                    >
+                        Connexion
+                    </button>
+                </div>
+            )}
+        </header>
+    );
 };
 
 export default Header;
