6b28c36078de61a9d00cfe55540ad441c28cb7f2
Tiphaine JUQUEL <Tiphaine.juquel@gmail.com>
2023-01-11 16:59:37 +0100

    [Front] - Modify CSS for base panel

--- src/components/UserInfosForm.jsx
+++ src/components/UserInfosForm.jsx
@@ -114,194 +114,224 @@ const UserInfosForm = (props) => {
   };
 
   return (
-    <form
-      style={{
-        display: "flex",
-        flexDirection: "column",
-      }}
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleSubmit();
-      }}
-    >
-      <label htmlFor="gender">civilite</label>
-      <select
-        name="gender-select"
-        id=""
-        value={userInfo.gender}
-        onChange={(e) => {
-          setUserInfo({ ...userInfo, gender: e.target.value });
-          console.log(e.target.value);
-        }}
-      >
-        <option value="male">Homme</option>
-        <option value="female">Femme</option>
-        <option value="other">Autre</option>
-      </select>
-      <label htmlFor="service">categorie</label>
-      <select
-        name="service-select"
-        id=""
-        value={userInfo.service}
-        onChange={(e) => {
-          setUserInfo({ ...userInfo, service: e.target.value });
-          console.log(e.target.value);
-        }}
-      >
-        <option value="Technique">Technique</option>
-        <option value="Marketing">Marketing</option>
-        <option value="Client">Client</option>
-      </select>
-      <label htmlFor="firstname">Prénom</label>
-      <input
-        type="text"
-        name="firstname"
-        id="firstname"
-        value={userInfo.firstname}
-        onChange={(e) => {
-          setUserInfo({ ...userInfo, firstname: e.target.value });
-          console.log(e.target.value);
-        }}
-      />
-      <label htmlFor="lastname">Nom</label>
-      <input
-        type="text"
-        name="lastname"
-        id="lastname"
-        value={userInfo.lastname}
-        onChange={(e) => {
-          setUserInfo({ ...userInfo, lastname: e.target.value });
-          console.log(e.target.value);
-        }}
-      />
-      <label htmlFor="email">Email</label>
-      <input
-        type="text"
-        name="email"
-        id="email"
-        value={userInfo.email}
-        onChange={(e) => {
-          setUserInfo({ ...userInfo, email: e.target.value });
-          console.log(e.target.value);
-        }}
-      />
-      <label htmlFor="phone">Téléphone</label>
-      <input
-        type="text"
-        name="phone"
-        id="phone"
-        value={userInfo.phone}
-        onChange={(e) => {
-          setUserInfo({ ...userInfo, phone: e.target.value });
-          console.log(e.target.value);
-        }}
-      />
-      <label htmlFor="password">Mot de passe</label>
-      <p
-        style={{
-          color: "red",
-        }}
-      >
-        {passwordCheckText}
-      </p>
-      <input
-        type="text"
-        name="password"
-        id="password"
-        placeholder="Changer le mot de passe"
-        onChange={(e) => {
-          setUserInfo({ ...userInfo, tempPassword: e.target.value });
-          console.log(e.target.value);
-          if (e.target.value === userInfo.password) {
-            setPasswordCheckText("");
-          } else {
-            setPasswordCheckText("Les mots de passe doivent correspondre");
-          }
-        }}
-      />
-      <label htmlFor="confirm-password">Confirmer le mot de passe</label>
-      <input
-        type="text"
-        name="password"
-        id="password"
-        onChange={(e) => {
-          setUserInfo({ ...userInfo, password: e.target.value });
-          if (e.target.value === userInfo.tempPassword) {
-            console.log(e.target.value);
-            setPasswordCheckText("");
-          } else {
-            setPasswordCheckText("Les mots de passe doivent correspondre");
-          }
-        }}
-      />
-      <p
-        style={{
-          color: "red",
-        }}
-      >
-        {passwordCheckText}
-      </p>
-      <label htmlFor="birthdate">Date de naissance</label>
-      <input
-        type="date"
-        name="birthdate"
-        id="birthdate"
-        value={userInfo.birthdate}
-        onChange={(e) => {
-          setUserInfo({ ...userInfo, birthdate: e.target.value });
-          console.log(e.target.value);
-        }}
-      />
-      <label htmlFor="city">Ville</label>
-      <input
-        type="text"
-        name="address"
-        id=""
-        value={userInfo.city}
-        onChange={(e) => {
-          setUserInfo({ ...userInfo, city: e.target.value });
-          console.log(e.target.value);
-        }}
-      />
-      <label htmlFor="country">Pays</label>
-      <input
-        type="text"
-        name="country"
-        id="country"
-        value={userInfo.country}
-        onChange={(e) => {
-          setUserInfo({ ...userInfo, country: e.target.value });
-          console.log(e.target.value);
-        }}
-      />
-      <label htmlFor="photo">URL de la photo</label>
-      <input
-        type="text"
-        name="photo"
-        id="photo"
-        value={userInfo.photo}
-        onChange={(e) => {
-          setUserInfo({ ...userInfo, photo: e.target.value });
-          console.log(e.target.value);
-        }}
-      />
-      {user.isAdmin && (
-        <>
-          <label htmlFor="isAdmin">Admin</label>
-          <input
-            type="checkbox"
-            name="isAdmin"
-            id="isAdmin"
-            checked={userInfo.isAdmin}
-            onChange={(e) => {
-              setUserInfo({ ...userInfo, isAdmin: e.target.checked });
-              console.log(e.target.checked);
-            }}
-          />
-        </>
-      )}
-      <button type="submit">{props.type}</button>
-    </form>
+      <div className="flex justify-center w-72">
+          <form className="flex flex-col ml-5 text-black"
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    handleSubmit();
+                }}
+          >
+              <div className="flex flex-nowrap mb-2">
+                  <label htmlFor="gender">Civilité</label>
+                  <select className="p-2 ml-2 rounded w-2/3 text-white"
+                          name="gender-select"
+                          id=""
+                          value={userInfo.gender}
+                          onChange={(e) => {
+                              setUserInfo({ ...userInfo, gender: e.target.value });
+                              console.log(e.target.value);
+                          }}
+                  >
+                      <option value="male">Homme</option>
+                      <option value="female">Femme</option>
+                      <option value="other">Autre</option>
+                  </select>
+              </div>
+
+              <div className="flex flex-nowrap mb-2">
+                  <label htmlFor="service">Catégorie</label>
+                  <select className="p-2 ml-2 w-2/3 rounded text-white"
+                          name="service-select"
+                          id=""
+                          value={userInfo.service}
+                          onChange={(e) => {
+                              setUserInfo({ ...userInfo, service: e.target.value });
+                              console.log(e.target.value);
+                          }}
+                  >
+                      <option value="Technique">Technique</option>
+                      <option value="Marketing">Marketing</option>
+                      <option value="Client">Client</option>
+                  </select>
+              </div>
+
+              <div className="flex flex-nowrap mb-2">
+                  <label htmlFor="firstname">Prénom</label>
+                  <input className="p-2 ml-2 w-4/5 rounded-md text-white"
+                         type="text"
+                         name="firstname"
+                         id="firstname"
+                         value={userInfo.firstname}
+                         onChange={(e) => {
+                             setUserInfo({ ...userInfo, firstname: e.target.value });
+                             console.log(e.target.value);
+                         }}
+                  />
+              </div>
+
+              <div className="flex flex-nowrap mb-2">
+                  <label htmlFor="lastname">Nom</label>
+                  <input className="p-2 ml-2 w-4/5 rounded-md text-white"
+                         type="text"
+                         name="lastname"
+                         id="lastname"
+                         value={userInfo.lastname}
+                         onChange={(e) => {
+                             setUserInfo({ ...userInfo, lastname: e.target.value });
+                             console.log(e.target.value);
+                         }}
+                  />
+              </div>
+
+              <div className="flex flex-nowrap mb-2">
+                  <label htmlFor="email">Email</label>
+                  <input className="p-2 ml-2 w-4/5 rounded-md text-white"
+                         type="text"
+                         name="email"
+                         id="email"
+                         value={userInfo.email}
+                         onChange={(e) => {
+                             setUserInfo({ ...userInfo, email: e.target.value });
+                             console.log(e.target.value);
+                         }}
+                  />
+              </div>
+
+              <div className="flex flex-nowrap mb-2">
+                  <label htmlFor="phone">Téléphone</label>
+                  <input className="p-2 ml-2 w-4/5 rounded-md text-white"
+                         type="text"
+                         name="phone"
+                         id="phone"
+                         value={userInfo.phone}
+                         onChange={(e) => {
+                             setUserInfo({ ...userInfo, phone: e.target.value });
+                             console.log(e.target.value);
+                         }}
+                  />
+              </div>
+
+              <div className="flex flex-nowrap mb-2">
+                  <label htmlFor="password">Mot de passe</label>
+                  <p className="text-red-700">
+                      {passwordCheckText}
+                  </p>
+                  <input className="p-2 ml-2 w-4/5 rounded-md text-white"
+                         type="text"
+                         name="password"
+                         id="password"
+                         placeholder="Changer le mot de passe"
+                         onChange={(e) => {
+                             setUserInfo({ ...userInfo, tempPassword: e.target.value });
+                             console.log(e.target.value);
+                             if (e.target.value === userInfo.password) {
+                                 setPasswordCheckText("");
+                             } else {
+                                 setPasswordCheckText("Les mots de passe doivent correspondre");
+                             }
+                         }}
+                  />
+              </div>
+
+              <div className="flex flex-nowrap mb-2">
+                  <label htmlFor="confirm-password">Confirmer le mot de passe</label>
+                  <input className="p-2 ml-2 w-4/5 rounded-md text-white"
+                         type="text"
+                         name="password"
+                         id="password"
+                         onChange={(e) => {
+                             setUserInfo({ ...userInfo, password: e.target.value });
+                             if (e.target.value === userInfo.tempPassword) {
+                                 console.log(e.target.value);
+                                 setPasswordCheckText("");
+                             } else {
+                                 setPasswordCheckText("Les mots de passe doivent correspondre");
+                             }
+                         }}
+                  />
+                  <p className="text-red-600">
+                      {passwordCheckText}
+                  </p>
+              </div>
+
+              <div className="flex flex-nowrap mb-2">
+                  <label htmlFor="birthdate">Date de naissance</label>
+                  <input className="p-2 ml-2 w-4/5 rounded-md text-white"
+                         type="date"
+                         name="birthdate"
+                         id="birthdate"
+                         value={userInfo.birthdate}
+                         onChange={(e) => {
+                             setUserInfo({ ...userInfo, birthdate: e.target.value });
+                             console.log(e.target.value);
+                         }}
+                  />
+              </div>
+
+              <div className="flex flex-nowrap mb-2">
+                  <label htmlFor="city">Ville</label>
+                  <input className="p-2 ml-2 w-4/5 rounded text-white"
+                         type="text"
+                         name="address"
+                         id=""
+                         value={userInfo.city}
+                         onChange={(e) => {
+                             setUserInfo({ ...userInfo, city: e.target.value });
+                             console.log(e.target.value);
+                         }}
+                  />
+              </div>
+
+              <div className="flex flex-nowrap mb-2">
+                  <label htmlFor="country">Pays</label>
+                  <input className="p-2 ml-2 w-4/5 rounded text-white"
+                         type="text"
+                         name="country"
+                         id="country"
+                         value={userInfo.country}
+                         onChange={(e) => {
+                             setUserInfo({ ...userInfo, country: e.target.value });
+                             console.log(e.target.value);
+                         }}
+                  />
+              </div>
+
+              <div className="flex flex-nowrap mb-2">
+                  <label htmlFor="photo">URL de la photo</label>
+                  <input className="p-2 ml-2 w-4/5 rounded text-white"
+                         type="text"
+                         name="photo"
+                         id="photo"
+                         value={userInfo.photo}
+                         onChange={(e) => {
+                             setUserInfo({ ...userInfo, photo: e.target.value });
+                             console.log(e.target.value);
+                         }}
+                  />
+              </div>
+
+              {user.isAdmin && (
+                  <>
+                      <div className="flex flex-nowrap mb-2">
+                          <label htmlFor="isAdmin">Admin</label>
+                          <input className="p-2 ml-2 w-4/5 text-white"
+                              type="checkbox"
+                              name="isAdmin"
+                              id="isAdmin"
+                              checked={userInfo.isAdmin}
+                              onChange={(e) => {
+                                  setUserInfo({ ...userInfo, isAdmin: e.target.checked });
+                                  console.log(e.target.checked);
+                              }}
+                          />
+                      </div>
+                  </>
+              )}
+              <button className="m-3 rounded-lg h-12 font-bold text-white bg-gradient-to-b from-red-400 via-red-500 to-pink-500"
+                  type="submit">{props.type}</button>
+          </form>
+      </div>
+
   );
 };
 
