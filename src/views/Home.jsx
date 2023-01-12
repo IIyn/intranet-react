76b0c7245fc51bd781a8fb29a167e9b4f3a4ab6c
Tiphaine JUQUEL <Tiphaine.juquel@gmail.com>
2023-01-12 11:23:28 +0100

    [Front] - Refactor code style and modify CSS for button random users

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,72 +1,74 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 
-import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { selectUser } from "../slices/userSlice";
+import {useNavigate} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {selectUser} from "../slices/userSlice";
 import getRandomCollaborator, {
-  deleteCollaborator,
+    deleteCollaborator,
 } from "../services/collaboratorsManagement";
 
 import ProfileCard from "../components/ProfileCard";
 
 const Register = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const [randomUser, setRandomUser] = useState(null);
-  const [reload, setReload] = useState(false);
-  const { user } = useSelector(selectUser);
+    const [randomUser, setRandomUser] = useState(null);
+    const [reload, setReload] = useState(false);
+    const {user} = useSelector(selectUser);
 
-  useEffect(() => {
-    if (localStorage.getItem("user") === null) {
-      navigate("/login");
-    }
-    getRandomCollaborator()
-      .then((res) => {
-        console.log("RESPONSE OK ", res.data);
-        setRandomUser(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [reload]);
-
-  return (
-    <div className="home">
-      {user && (
-        <button
-          onClick={() => {
-            getRandomCollaborator()
-              .then((res) => {
+    useEffect(() => {
+        if (localStorage.getItem("user") === null) {
+            navigate("/login");
+        }
+        getRandomCollaborator()
+            .then((res) => {
                 console.log("RESPONSE OK ", res.data);
                 setRandomUser(res.data);
-              })
-              .catch((err) => {
+            })
+            .catch((err) => {
                 console.log(err);
-              });
-          }}
-        >
-          random user
-        </button>
-      )}
+            });
+    }, [reload]);
 
-      {randomUser && (
-        <ProfileCard 
-          randomUser={randomUser}
-          deleteUser={() => {
-            deleteCollaborator(randomUser.id)
-              .then((res) => {
-                // console.log(res);
-                setReload(!reload);
-                se;
-              })
-              .catch((err) => {
-                console.log(err);
-              });
-          }}
-        />
-      )}
-    </div>
-  );
+    return (
+        <div className="home">
+            {user && (
+                <div className="flex justify-center p-5 my-5">
+                    <button className="font-bold text-3xl p-3 rounded-full bg-gradient-to-b from-red-400 via-red-500 to-pink-500"
+                            onClick={() => {
+                                getRandomCollaborator()
+                                    .then((res) => {
+                                        console.log("RESPONSE OK ", res.data);
+                                        setRandomUser(res.data);
+                                    })
+                                    .catch((err) => {
+                                        console.log(err);
+                                    });
+                            }}
+                    >
+                        random user
+                    </button>
+                </div>
+
+            )}
+            {randomUser && (
+                <ProfileCard
+                    randomUser={randomUser}
+                    deleteUser={() => {
+                        deleteCollaborator(randomUser.id)
+                            .then((res) => {
+                                // console.log(res);
+                                setReload(!reload);
+                                se;
+                            })
+                            .catch((err) => {
+                                console.log(err);
+                            });
+                    }}
+                />
+            )}
+        </div>
+    );
 };
 
 export default Register;
